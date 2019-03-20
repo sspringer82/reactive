b0d76bb71874cdfa564595524ac406a1e5d77198
Sebastian Springer <info@sebastian-springer.com>
2019-03-20 11:44:41 +0100

    aufgabe5

---
+++ Aufgabe5/index.js
@@ -0,0 +1,32 @@
+const { timer, interval } = require('rxjs');
+const { map, filter, withLatestFrom, take } = require('rxjs/operators');
+const { Observable } = require('rxjs');
+
+const t = timer(0, 1000).pipe(map(() => new Date().toString()));
+const strings = ['bla', 'blubb', 'foo', 'bar', 'baz'];
+
+/*const messages = Observable.create(observer => {
+  setInterval(() => {
+    if (Date.now() % 5 === 0) {
+      observer.next(strings[Math.floor(Math.random() * strings.length)]);
+    }
+  },500);
+});*/
+
+const messages = interval(10).pipe(
+  filter(v => {
+    return v % Math.floor(Math.random() * 5) === 0;
+  }),
+  map(() => {
+    return strings[Math.floor(Math.random() * strings.length)];
+  })
+);
+
+messages
+  .pipe(
+    withLatestFrom(t),
+    take(10)
+  )
+  .subscribe(v => {
+    console.log(v[1] + ' ' + v[0]);
+  });
